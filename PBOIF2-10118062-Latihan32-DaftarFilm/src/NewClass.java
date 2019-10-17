484bd1e872a5072a54a2f07dbdd70024cdc61c0d
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-10-17 21:56:21 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan32-DaftarFilm/src/NewClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class NewClass {
+
+    String nama;
+    String genre;
+    Double rating;
+    int duration;
+    
+    public void nowPlaying(String genre,String nama , Double rating ,int duration){
+     System.out.println("Hallo everyone ");
+     System.out.println("Judul Film: " +nama );
+     System.out.println("Genre Film : "+genre);
+     System.out.println("Rating Film : "+rating);
+     System.out.println("Duration Film : "+duration+ " Menit");
+     System.out.println("");
+     System.out.println("Developed by satyaning andaru");
+    
+    }
+    
+}
